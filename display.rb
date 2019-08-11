2b47cfeb1f03b0a8ad6d57bd68bf72b2286d1404
rdeyvil <rdeyvil11@gmail.com>
2019-08-12 01:26:54 +0545

    Add class and methods

---
+++ display.rb
@@ -0,0 +1,12 @@
+
+class Display 
+  
+  def self.display(response_hash)
+    #Hash of Array of Hashes 
+    binding.pry
+    response_hash["results"].each_with_index do |place, i|
+      puts "#{i+1}. #{place["name"]}"
+    end
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'uri'
+require 'pry'
+require 'open-uri'
+
+require 'dotenv'
+
+Dotenv.load('.env') #loads ENV variables from .env file
+
+require_relative 'current_location'
+require_relative 'display'
+require_relative 'locator'
+require_relative 'user_input'
+
+user_info = UserInput.new
+location = CurrentLocation.new
+locator = Locator.new(user_info, location)
+
+Display.display(locator.search)
